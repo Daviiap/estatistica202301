287faf4a6fec124832c11f5a98c82641c36f737c
felipe <felipe.siqueira@ccc.ufcg.edu.br>
2023-11-04 15:56:33 -0300

    concluindo testes de normalidade e de correlação

--- main.r
+++ main.r
@@ -1,1 +1,18 @@
 data <- Orange
+
+# Análise de aderência e associação
+
+# Teste de normalidade para variável resposta (teste Shapiro-Wilk)
+# Considerando que a variável resposta é age
+
+shapiro_test <- shapiro.test(data$age)
+print(shapiro_test)
+# Como o valor-p é menor que 0.05, rejeitamos a hipótese nula de que a variável resposta segue uma distribuição normal
+
+
+# Calculando o coeficiente de correlação de Pearson entre as variáveis age e circumference
+
+correlation <- cor.test(data$age, data$circumference, method = "pearson")
+print(correlation)
+
+
