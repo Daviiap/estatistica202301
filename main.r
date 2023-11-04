466656fe9bd719d2127b380e731eb8539dcc16b2
davi.pontes <davi.pontes@lsd.ufcg.edu.br>
2023-11-04 12:44:43 -0300

    teste de hipotese para media

--- main.r
+++ main.r
@@ -1,22 +1,19 @@
 # Definindo dataset
 data <- Orange
 
-# Calcular a estimativa para a média da idade (variável resposta)
-estimativa_media <- mean(data$age)
+# Definindo as estimativas para a média
+# Pontual
+estimativa_pontual_media <- mean(data$age)
+# Intervalo de confiança
+estimativa_intervalo_media <- t.test(data$age, conf.level = 0.95)
 
-# Calcular a estimativa para a variância da idade (variável resposta)
-estimativa_variancia <- var(data$age)
+print(estimativa_pontual_media)
+print(estimativa_intervalo_media$conf.int[1])
+print(estimativa_intervalo_media$conf.int[2])
 
-# Definindo valor de teste para a variancia
-valor_teste_media <- 920
+teste_hip_media <- t.test(data$age,
+                          alternative = "two.sided",
+                          mu = 920,
+                          confident.interval = 0.95)
 
-# Resultado para o teste de hipotese da média
-resultado_teste_media <- t.test(data$age, mu = valor_teste_media)
-
-print(resultado_teste_media)
-
-
-modelo <- lm(age ~ circumference, data = data)
-coeficientes <- coef(modelo)
-var_erro <- summary(modelo)$sigma^2
-confint(modelo)
+print(teste_hip_media$p.value)
